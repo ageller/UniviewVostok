e7ca0d3debf415489d135d5c67c42e1191a03bd5
Aaron Geller <a-geller@northwestern.edu>
2018-04-13 15:06:16 -0500

    added CO2 red to fragment

--- VostokIceAges/data/vostok.fs
+++ VostokIceAges/data/vostok.fs
@@ -4,10 +4,14 @@ uniform float uv_alpha;
 uniform float simUseTime;
 uniform float ntAve;
 uniform float tAveRange;
-uniform sampler3D glacierTex;
-uniform sampler2D co2Tex;
+uniform float CO2lim;
+uniform bool showCO2;
 
-uniform vec2 co2range;
+uniform sampler3D glacierTex;
+uniform sampler2D CO2Tex;
+
+//parameters
+uniform vec2 CO2range;
 uniform vec2 timerange;
 
 in vec2 TexCoord;
@@ -19,23 +23,30 @@ void main()
 
 	//get the CO2 level from the Vostok ice coore
 	float time = (simUseTime - timerange[0])/(timerange[1] - timerange[0]);
-	vec4 co2val = texture(co2Tex, vec2(time, 0.5));
+	vec4 CO2val = texture(CO2Tex, vec2(time, 0.5));
 	if (ntAve > 0){
-		co2val = vec4(0.);
+		CO2val = vec4(0.);
 		float offset = 0.;
 		for (int i=0; i<2*ntAve; i++){
 			offset = (i - ntAve)/ntAve*tAveRange;
 			time = (simUseTime + offset - timerange[0])/(timerange[1] - timerange[0]);
-			co2val += texture(co2Tex, vec2(time, 0.5));
+			CO2val += texture(CO2Tex, vec2(time, 0.5));
 		}
-		co2val /= (2*ntAve);
+		CO2val /= (2*ntAve);
 	}
-	float c = co2val.r*256. + 100 + co2val.g;
+	float c = CO2val.r*256. + 100 + CO2val.g;
 
 	//find the position in the glacier texture for FragColor
-	float zpos = (c - co2range[0])/(co2range[1] - co2range[0]);
+	float zpos = (c - CO2range[0])/(CO2range[1] - CO2range[0]);
 	vec4 color = texture(glacierTex, vec3(TexCoord, zpos));
 	color.a = uv_alpha*uv_fade;
 
+	float CO2color = 0.;
+	if (showCO2){
+		CO2color = (1. - zpos)*CO2lim;
+	}
+	color.r += CO2color;
+
 	FragColor = color;
+	//FragColor = vec4(zpos, 0, 0, 1);
 }
