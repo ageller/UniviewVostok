6575164c45b248487e2a38157e8b09aede3703e3
Aaron Geller <ageller@williamson.local>
2018-04-18 14:44:14 -0500

    clean up

--- VostokIceAges/data/earthCO2.fs
+++ VostokIceAges/data/earthCO2.fs
@@ -21,14 +21,14 @@ void main()
 {	
 
 	//get the CO2 level from the Vostok ice coore
-	float time = (simUseTime - timerange[0])/(timerange[1] - timerange[0]);
+	float time = clamp((simUseTime - timerange[0])/(timerange[1] - timerange[0]), 0, 1);
 	vec4 CO2val = texture(CO2Tex, vec2(time, 0.5));
 	if (ntAve > 0){
 		CO2val = vec4(0.);
 		float offset = 0.;
 		for (int i=0; i<2*ntAve; i++){
 			offset = (i - ntAve)/ntAve*tAveRange;
-			time = (simUseTime + offset - timerange[0])/(timerange[1] - timerange[0]);
+			time = clamp((simUseTime + offset - timerange[0])/(timerange[1] - timerange[0]), 0, 1);
 			CO2val += texture(CO2Tex, vec2(time, 0.5));
 		}
 		CO2val /= (2*ntAve);
@@ -36,8 +36,8 @@ void main()
 	float c = CO2val.r*256. + 100 + CO2val.g;
 
 	//find the position in the glacier texture for FragColor
-	float zpos = (c - CO2range[0])/(CO2range[1] - CO2range[0]);
-
+	float zpos = clamp(1. - (c - CO2range[0])/(CO2range[1] - CO2range[0]),0.001,0.999);
+	
 	float CO2color = 0.;
 	if (showCO2){
 		CO2color = (1. - zpos)*CO2lim;
