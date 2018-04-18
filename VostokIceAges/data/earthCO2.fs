67a9b4db52fdd89aa510dc40e5dd01575d0a9315
Aaron Geller <a-geller@northwestern.edu>
2018-04-18 16:17:29 -0500

    added future glacier and CO2

--- VostokIceAges/data/earthCO2.fs
+++ VostokIceAges/data/earthCO2.fs
@@ -13,35 +13,48 @@ uniform float CO2lim;
 
 //parameters
 uniform vec2 CO2range;
-uniform vec2 timerange;
+uniform vec2 pastTimerange;
+uniform vec2 futureTimerange;
 
 out vec4 FragColor;
 
 void main()
 {	
-
-	//get the CO2 level from the Vostok ice coore
-	float time = clamp((simUseTime - timerange[0])/(timerange[1] - timerange[0]), 0, 1);
-	vec4 CO2val = texture(CO2Tex, vec2(time, 0.5));
-	if (ntAve > 0){
-		CO2val = vec4(0.);
-		float offset = 0.;
-		for (int i=0; i<2*ntAve; i++){
-			offset = (i - ntAve)/ntAve*tAveRange;
-			time = clamp((simUseTime + offset - timerange[0])/(timerange[1] - timerange[0]), 0, 1);
-			CO2val += texture(CO2Tex, vec2(time, 0.5));
-		}
-		CO2val /= (2*ntAve);
-	}
-	float c = CO2val.r*256. + 100 + CO2val.g;
-
-	//find the position in the glacier texture for FragColor
-	float zpos = clamp(1. - (c - CO2range[0])/(CO2range[1] - CO2range[0]),0.001,0.999);
-	
 	float CO2color = 0.;
-	if (showCO2){
-		CO2color = (1. - zpos)*CO2lim;
-	}	
+
+	if (simUseTime < 0){
+		//get the CO2 level from the Vostok ice coore
+		float time = clamp((simUseTime - pastTimerange[0])/(pastTimerange[1] - pastTimerange[0]), 0, 1);
+		vec4 CO2val = texture(CO2Tex, vec2(time, 0.5));
+		if (ntAve > 0){
+			CO2val = vec4(0.);
+			float offset = 0.;
+			for (int i=0; i<2*ntAve; i++){
+				offset = (i - ntAve)/ntAve*tAveRange;
+				time = clamp((simUseTime + offset - pastTimerange[0])/(pastTimerange[1] - pastTimerange[0]), 0, 1);
+				CO2val += texture(CO2Tex, vec2(time, 0.5));
+			}
+			CO2val /= (2*ntAve);
+		}
+		float c = CO2val.r*256. + 100 + CO2val.g;
+
+		//find the position in the glacier texture for FragColor
+		float zpos = clamp(1. - (c - CO2range[0])/(CO2range[1] - CO2range[0]),0.001,0.999);
+		
+		if (showCO2){
+			CO2color = (1. - zpos)*CO2lim;
+		}	
+	} else {
+		float time = clamp((simUseTime - futureTimerange[0])/(futureTimerange[1] - futureTimerange[0]), 0, 1);
+		float c = 0.;
+		if (simUseTime <= 2100){
+			c = 3.85 * simUseTime - 7341.;
+		} else {
+			c = 240.*exp(-0.002*(simUseTime - 2100.)) + 500.;
+		}
+		CO2color = clamp((c - 360)/(745 - 360), 0 , 1)*CO2lim;
+	}
+	
 	FragColor = vec4(CO2color, 0., 0., CO2color);
 	FragColor.a *= uv_fade * uv_alpha;
 
